78cd177177206706dc7a797de39b00a70000229d
Evans <manotievance@gmail.com>
2023-09-18 21:52:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser = 'Evans Manoti';
+const welcomeMessage = 'Welcome to Flatbook, currentUser=Evans Manoti!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = 'Welcome, E!';
